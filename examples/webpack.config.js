dd2a8d6af58a708697f88add34996027ed94e43c
Aaron Franks <aaron.franks@gmail.com>
2017-09-15 21:14:36 -0700

    Webpack: add bundle-analyzer and source maps

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+const {ANALYZE_BUNDLE} = process.env
+
 
 module.exports = {
   entry: {
     lineChart: '../lib/es6/examples/lineChart.js',
   },
+
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
   },
+
+  plugins: ANALYZE_BUNDLE ? [new BundleAnalyzerPlugin()] : [],
+
+  devtool: 'cheap-eval-source-map'
 }
 
