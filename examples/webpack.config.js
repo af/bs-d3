fb1955323651170aad466a875c32787e2a913eb3
Aaron Franks <aaron.franks@gmail.com>
2017-09-16 08:33:04 -0700

    Prepare webpack config for multiple example entries

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,20 +1,27 @@
+const fs = require('fs')
 const path = require('path')
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 const {ANALYZE_BUNDLE} = process.env
 
 
+// Generate a separate entry script for each example source file:
+const sourceDir = path.join(__dirname, '..', 'lib', 'es6', 'examples')
+const exampleSources = fs.readdirSync(sourceDir)
+const entry = exampleSources.reduce((acc, src) => {
+    const entryName = src.split('.')[0]
+    const fullPath = path.join(sourceDir, src)
+    return {...acc, [entryName]: fullPath}
+}, {})
+
 module.exports = {
-  entry: {
-    lineChart: '../lib/es6/examples/lineChart.js',
-  },
+    entry,
 
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
-  },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+    },
 
-  plugins: ANALYZE_BUNDLE ? [new BundleAnalyzerPlugin()] : [],
+    plugins: ANALYZE_BUNDLE ? [new BundleAnalyzerPlugin()] : [],
 
-  devtool: 'cheap-eval-source-map'
+    devtool: 'cheap-eval-source-map'
 }
-
